d42b2413fa6cef4b68c6b62dadaa59d7700a2ba2
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-04-03 14:55:02 -0500

    layering imgs

--- src/App.js
+++ src/App.js
@@ -3,7 +3,16 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <h1>Hey</h1>
+      <img
+        className="dog"
+        alt="dog sitting down"
+        src="https://media.istockphoto.com/photos/australian-shepherd-sitting-against-white-background-picture-id1154953522?k=6&m=1154953522&s=612x612&w=0&h=fC_mXUBNrityucPbe2NOfcNj9jJr1H5v5ce37xqiRuQ="
+      />
+      <img
+        className="shirt-1"
+        alt="shirt"
+        src="https://cache.mrporter.com/variants/images/560971903912856/in/w1200_q80.jpg"
+      />
     </div>
   );
 }
