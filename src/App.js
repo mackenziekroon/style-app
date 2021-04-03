d0845f100c1c91bf07bd496359b4fdd84846f292
Mackenzie Kroon <kroonmackenzie@gmail.com>
2021-04-03 15:10:00 -0500

    css dog

--- src/App.js
+++ src/App.js
@@ -3,16 +3,23 @@ import "./App.css";
 function App() {
   return (
     <div className="App">
-      <img
-        className="dog"
-        alt="dog sitting down"
-        src="https://media.istockphoto.com/photos/australian-shepherd-sitting-against-white-background-picture-id1154953522?k=6&m=1154953522&s=612x612&w=0&h=fC_mXUBNrityucPbe2NOfcNj9jJr1H5v5ce37xqiRuQ="
-      />
-      <img
-        className="shirt-1"
-        alt="shirt"
-        src="https://cache.mrporter.com/variants/images/560971903912856/in/w1200_q80.jpg"
-      />
+      <div className="dog-container">
+        <img
+          className="dog"
+          alt="dog sitting down"
+          src="https://media.istockphoto.com/photos/australian-shepherd-sitting-against-white-background-picture-id1154953522?k=6&m=1154953522&s=612x612&w=0&h=fC_mXUBNrityucPbe2NOfcNj9jJr1H5v5ce37xqiRuQ="
+        />
+        {/* <img
+          className="shirt-1"
+          alt="shirt"
+          src="https://cache.mrporter.com/variants/images/560971903912856/in/w1200_q80.jpg"
+        />
+        <img
+          className="hat-1"
+          alt="hat"
+          src="https://cache.mrporter.com/variants/images/30049528927182865/in/w1200_q80.jpg"
+        /> */}
+      </div>
     </div>
   );
 }
